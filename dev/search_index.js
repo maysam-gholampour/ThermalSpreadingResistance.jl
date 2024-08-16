a80049a0217bf17082fb8c16dae598060e2ac0a9
Documenter.jl <documenter@juliadocs.github.io>
2024-08-16 11:50:37 +0000

    build based on d328345

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ThermalSpreadingResistance","category":"page"},{"location":"#ThermalSpreadingResistance","page":"Home","title":"ThermalSpreadingResistance","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ThermalSpreadingResistance.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ThermalSpreadingResistance]","category":"page"}]
+}
