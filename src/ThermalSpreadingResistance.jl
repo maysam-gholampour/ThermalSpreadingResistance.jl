59ed99d02a701a37800c7fbfaebfe52a306c2973
maysam-gholampour <meysam.gholampoor@gmail.com>
2024-08-16 20:22:44 +0800

    add precompiling

--- src/ThermalSpreadingResistance.jl
+++ src/ThermalSpreadingResistance.jl
@@ -1,5 +1,25 @@
 module ThermalSpreadingResistance
 
+    using PrecompileTools: @setup_workload, @compile_workload  
+
     include("equations/equations.jl")
+
+    @setup_workload begin
+        a = 0.05
+        b = 0.05
+        c = 0.025
+        d = 0.025
+        Q = 800.0
+        𝑘ₛ = 200.0
+        δₛ = 0.001
+        𝑘ₚ = 5.0
+        δₚ = 0.0001
+        hᶜ = 150000.0
+        @compile_workload begin
+            sol = solve(Isotropic(),a,b,c,d,Q,𝑘ₛ,δₛ,hᶜ,100)
+
+            sol = solve(Compound(),a,b,c,d,Q,𝑘ₛ,δₛ,𝑘ₚ,δₚ,hᶜ,100)
+        end
+    end
 
 end
