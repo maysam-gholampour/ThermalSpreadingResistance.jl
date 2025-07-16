941a1b3d5834ccaf538b9d4f2d17267cab25133a
Roger <rogermb.en13@nycu.edu.tw>
2025-07-16 16:38:06 +0800

    Uncommented the Compound solution in the workload compilation

--- src/ThermalSpreadingResistance.jl
+++ src/ThermalSpreadingResistance.jl
@@ -22,7 +22,7 @@ include("equations/equations.jl")
     @compile_workload begin
         sol = solve(Isotropic(), a, b, c, d, Q, 𝑘ₛ, δₛ, hᶜ, Xᶜ, Yᶜ, 100)
 
-        #sol = solve(Compound(), a, b, c, d, Q, 𝑘ₛ, δₛ, 𝑘ₚ, δₚ, hᶜ, Xᶜ, Yᶜ, 100)
+        sol = solve(Compound(), a, b, c, d, Q, 𝑘ₛ, δₛ, 𝑘ₚ, δₚ, hᶜ, Xᶜ, Yᶜ, 100)
     end
 end
 
